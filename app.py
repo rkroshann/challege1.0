7fd572d3cb037f9b7bac413312560a872994217b
Roshan kumar s <amroshanrk@gmail.com>
2026-02-19 10:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, abort
+from flask import Flask, session, abort, render_template
 
 app = Flask(__name__)
 app.secret_key = "Mczjsn6"
@@ -7,14 +7,15 @@ app.secret_key = "Mczjsn6"
 def index():
     session["role"] = "user"
     session["user"] = "guest"
-    return "Welcome to the Animus"
+    return render_template("index.html")
 
 @app.route("/admin")
 def admin():
     if session.get("role") == "admin" and session.get("user") == app.secret_key[::-1]:
-        return "FLAG: vault{d35m0nd_m1l35}"
+        return render_template("admin.html")
     else:
         abort(403)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
+
